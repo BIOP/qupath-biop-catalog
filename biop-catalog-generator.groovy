c8818dacf4ced21ba00b01992515a26ea9605bd8
Rémy Dornier <remy.dornier@epfl.ch>
2025-07-08 11:23:24 +0200

    update qupath min version

--- biop-catalog-generator.groovy
+++ biop-catalog-generator.groovy
@@ -5,11 +5,10 @@ import java.nio.file.Paths
 
 def extensionList = []
 def savingFolder = "D:/Remy/Github-projects/qupath-biop-catalog/catalog.json"
+def qupathMinVersionRange = new VersionRange("v0.6.0", null, null)
 
 // cellpose extension
-def cellposeTagList = ["v0.11.0"] 
-def cellposeMinVersion = "v0.11.0"
-def cellposeVersionRange = new VersionRange(cellposeMinVersion, null, null)
+def cellposeTagList = ["v0.10.2"] 
 def cellposeVersionList = []
 cellposeTagList.each{tag->
     var cellposeRelease = new Release(
@@ -18,7 +17,7 @@ cellposeTagList.each{tag->
        null,
        null,
        null,
-       cellposeVersionRange
+       qupathMinVersionRange
     )
     
     cellposeVersionList.add(cellposeRelease)
@@ -34,18 +33,16 @@ var cellposeExtension = new Extension(
 extensionList.add(cellposeExtension)
 
 // abba extension
-def abbaTagList = ["v0.4.0"] 
-def abbaMinVersion = "v0.4.0"
-def abbaVersionRange = new VersionRange(abbaMinVersion, null, null)
+def abbaTagList = ["0.3.3"] 
 def abbaVersionList = []
 abbaTagList.each{tag->
     var abbaRelease = new Release(
-       tag,
-       new URI("https://github.com/BIOP/qupath-extension-abba/releases/download/"+tag+"/qupath-extension-abba-"+tag[1..-1]+".zip"),
+       "v"+tag,
+       new URI("https://github.com/BIOP/qupath-extension-abba/releases/download/"+tag+"/qupath-extension-abba-"+tag+".zip"),
        null,
        null,
        null,
-       abbaVersionRange
+       qupathMinVersionRange
     )
     
     abbaVersionList.add(abbaRelease)
@@ -62,18 +59,16 @@ extensionList.add(abbaExtension)
 
 
 // warpy extension
-def warpyTagList = ["v0.4.0"] 
-def warpyMinVersion = "v0.4.0"
-def warpyVersionRange = new VersionRange(warpyMinVersion, null, null)
+def warpyTagList = ["0.3.1"] 
 def warpyVersionList = []
 warpyTagList.each{tag->
     var warpyRelease = new Release(
-       tag,
-       new URI("https://github.com/BIOP/qupath-extension-warpy/releases/download/"+tag+"/qupath-extension-warpy-"+tag[1..-1]+".zip"),
+       "v"+tag,
+       new URI("https://github.com/BIOP/qupath-extension-warpy/releases/download/"+tag+"/qupath-extension-warpy-"+tag+".zip"),
        null,
        null,
        null,
-       warpyVersionRange
+       qupathMinVersionRange
     )
     
     warpyVersionList.add(warpyRelease)
@@ -91,8 +86,6 @@ extensionList.add(warpyExtension)
 
 // spotiflow extension
 def spotiflowTagList = ["v0.2.0"] 
-def spotiflowMinVersion = "v0.2.0"
-def spotiflowVersionRange = new VersionRange(spotiflowMinVersion, null, null)
 def spotiflowVersionList = []
 spotiflowTagList.each{tag->
     var spotiflowRelease = new Release(
@@ -101,7 +94,7 @@ spotiflowTagList.each{tag->
        null,
        null,
        null,
-       spotiflowVersionRange
+       qupathMinVersionRange
     )
     
     spotiflowVersionList.add(spotiflowRelease)
@@ -119,8 +112,6 @@ extensionList.add(spotiflowExtension)
 
 // hrm extension
 def hrmTagList = ["v1.1.0"] 
-def hrmMinVersion = "v1.1.0"
-def hrmVersionRange = new VersionRange(hrmMinVersion, null, null)
 def hrmVersionList = []
 def jsoupVersion = "1.15.4"
 def socVersion = "5.19.0"
@@ -132,7 +123,7 @@ hrmTagList.each{tag->
        List.of(new URI("https://maven.scijava.org/service/local/artifact/maven/redirect?r=central&g=org.jsoup&a=jsoup&v="+jsoupVersion+"&e=jar"), 
                new URI("https://github.com/GReD-Clermont/simple-omero-client/releases/download/"+socVersion+"/simple-omero-client-"+socVersion+".jar")),
        null,
-       hrmVersionRange
+       qupathMinVersionRange
     )
     
     hrmVersionList.add(hrmRelease)
@@ -150,8 +141,6 @@ extensionList.add(hrmExtension)
 
 // biop extension
 def biopTagList = ["v3.3.0"] 
-def biopMinVersion = "v3.3.0"
-def biopVersionRange = new VersionRange(biopMinVersion, null, null)
 def biopVersionList = []
 biopTagList.each{tag->
     var biopRelease = new Release(
@@ -160,7 +149,7 @@ biopTagList.each{tag->
        null,
        null,
        null,
-       biopVersionRange
+       qupathMinVersionRange
     )
     
     biopVersionList.add(biopRelease)
