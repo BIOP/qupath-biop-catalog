e8558542316157eedc31e99408f4b822fbd7ae46
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2025-08-08 15:59:45 +0200

    adding ABBA extension, refactoring a bit common urls

--- biop-catalog-generator.groovy
+++ biop-catalog-generator.groovy
@@ -1,20 +1,53 @@
-import qupath.ext.extensionmanager.core.catalog.*
-import com.google.gson.GsonBuilder
-import com.google.gson.FieldNamingPolicy
-import java.nio.file.Paths
+/**
+ * QuPath BIOP Extension Catalog Generator
+ * 
+ * Generates a JSON catalog file for QuPath extensions maintained by the BIOP team.
+ * The catalog is used by QuPath's extension manager for easy installation and management.
+ * 
+ * Included extensions:
+ * - Cellpose extension - Cell segmentation
+ * - ABBA extension - Atlas alignment  
+ * - Warpy extension - Image transformations
+ * - BIOP extension - Utility functions
+ * 
+ * Usage: Run in QuPath script editor. Update 'savingFolder' path as needed.
+ * Output: catalog.json file for use with QuPath extension manager
+ * 
+ * @author BIOP
+ * @version 1.0
+ * @requires QuPath v0.6.0+
+ */
 
-def extensionList = []
-//def savingFolder = "D:/Remy/Github-projects/qupath-biop-catalog/catalog.json"
-def savingFolder = "F:/IAGitLab/qupath-biop-catalog/catalog.json"
+// --------------- YOU NEED TO MODIFY THIS VARIABLE BEFORE RUNNING IT
+def savingFolder = "F:/IAGitLab/qupath-biop-catalog/catalog.json" // Nico
+//def savingFolder = "D:/Remy/Github-projects/qupath-biop-catalog/catalog.json" // Rémy
+
+
+// Versions for packages which are common between different extensions
+
+def imglib2_realtransform_version = "3.1.2"
+def imglib2_version = "5.12.0"
+def jitk_tps_version = "3.0.2"
+def ejml_version = "0.24"
+def jama_version = "1.0.2"
+
+def mvn_sj_url = "https://maven.scijava.org/service/local/repositories/releases/content/"
+def mvn_central_url = "https://maven.scijava.org/service/local/repositories/central/content"
+
+def gh_biop_url = "https://github.com/BIOP/"
+
 def qupathMinVersionRange = new VersionRange("v0.6.0", null, null)
 
-// cellpose extension
+def extensionList = []
+
+// ------------------------ CELLPOSE EXTENSION
+
 def cellposeTagList = ["v0.11.0"] 
 def cellposeVersionList = []
 cellposeTagList.each{tag->
     var cellposeRelease = new Release(
        tag,
-       new URI("https://github.com/BIOP/qupath-extension-cellpose/releases/download/"+tag+"/qupath-extension-cellpose-"+tag[1..-1]+".zip"),
+       new URI(gh_biop_url+"qupath-extension-cellpose/releases/download/"+tag+"/qupath-extension-cellpose-"+tag[1..-1]+".zip"),
        null,
        null,
        null,
@@ -27,21 +60,29 @@ var cellposeExtension = new Extension(
    "QuPath Cellpose extension",
    "QuPath extension to use Cellpose",
    "BIOP",
-   new URI("https://github.com/BIOP/qupath-extension-cellpose"),
+   new URI(gh_biop_url+"qupath-extension-cellpose"),
    false,
    cellposeVersionList
 )
 extensionList.add(cellposeExtension)
 
-/*
-// abba extension
-def abbaTagList = ["0.3.3"] 
+
+// ------------------------ ABBA EXTENSION
+
+def abbaTagList = ["0.4.0"] 
 def abbaVersionList = []
 abbaTagList.each{tag->
     var abbaRelease = new Release(
        "v"+tag,
-       new URI("https://github.com/BIOP/qupath-extension-abba/releases/download/"+tag+"/qupath-extension-abba-"+tag+".zip"),
-       null,
+                   new URI(gh_biop_url    +"qupath-extension-abba/releases/download/"          +tag+                          "/qupath-extension-abba-" +tag+                          ".jar"),
+           List.of(new URI(mvn_sj_url     +"net/imglib2/imglib2-realtransform/"                +imglib2_realtransform_version+"/imglib2-realtransform-" +imglib2_realtransform_version+".jar"), // real-transform
+                   new URI(mvn_central_url+"net/imglib2/imglib2/"                              +imglib2_version+              "/imglib2-"               +imglib2_version+              ".jar"), // imglib2
+                   new URI(mvn_sj_url     +"jitk/jitk-tps/"                                    +jitk_tps_version+             "/jitk-tps-"              +jitk_tps_version+             ".jar"), // jitk-tps
+                   new URI(mvn_central_url+"com/googlecode/efficient-java-matrix-library/ejml/"+ejml_version+                 "/ejml-"                  +ejml_version+                 ".jar"), // ejml
+                   new URI(mvn_central_url+"gov/nist/math/jama/"                               +jama_version+                 "/jama-"                  +jama_version+                 ".jar"), // jama
+                   new URI(gh_biop_url    +"qupath-extension-warpy/releases/download/"         +tag+                          "/qupath-extension-warpy-"+tag+                          ".jar")  // warpy
+      
+           ),
        null,
        null,
        qupathMinVersionRange
@@ -58,20 +99,19 @@ var abbaExtension = new Extension(
    abbaVersionList
 )
 extensionList.add(abbaExtension)
-*/
 
-// warpy extension
+// ------------------------ WARPY EXTENSION
 def warpyTagList = ["0.4.2"] 
 def warpyVersionList = []
 warpyTagList.each{tag->
     var warpyRelease = new Release(
        "v"+tag,
-       new URI("https://github.com/BIOP/qupath-extension-warpy/releases/download/"+tag+"/qupath-extension-warpy-"+tag+".jar"),
-           List.of(new URI("https://maven.scijava.org/service/local/repositories/releases/content/net/imglib2/imglib2-realtransform/3.1.2/imglib2-realtransform-3.1.2.jar"), // real-transform
-                   new URI("https://maven.scijava.org/service/local/repositories/central/content/net/imglib2/imglib2/5.12.0/imglib2-5.12.0.jar"), // imglib2
-                   new URI("https://maven.scijava.org/service/local/repositories/releases/content/jitk/jitk-tps/3.0.2/jitk-tps-3.0.2.jar"), // jitk-tps
-                   new URI("https://maven.scijava.org/service/local/repositories/central/content/com/googlecode/efficient-java-matrix-library/ejml/0.24/ejml-0.24.jar"), // ejml
-                   new URI("https://maven.scijava.org/service/local/repositories/central/content/gov/nist/math/jama/1.0.2/jama-1.0.2.jar") // jama
+                   new URI(gh_biop_url    +"qupath-extension-warpy/releases/download/"         +tag+                          "/qupath-extension-warpy-"+tag+                          ".jar"),
+           List.of(new URI(mvn_sj_url     +"net/imglib2/imglib2-realtransform/"                +imglib2_realtransform_version+"/imglib2-realtransform-" +imglib2_realtransform_version+".jar"), // real-transform
+                   new URI(mvn_central_url+"net/imglib2/imglib2/"                              +imglib2_version+              "/imglib2-"               +imglib2_version+              ".jar"), // imglib2
+                   new URI(mvn_sj_url     +"jitk/jitk-tps/"                                    +jitk_tps_version+             "/jitk-tps-"              +jitk_tps_version+             ".jar"), // jitk-tps
+                   new URI(mvn_central_url+"com/googlecode/efficient-java-matrix-library/ejml/"+ejml_version+                 "/ejml-"                  +ejml_version+                 ".jar"), // ejml
+                   new URI(mvn_central_url+"gov/nist/math/jama/"                               +jama_version+                 "/jama-"                  +jama_version+                 ".jar")  // jama
            ),
        null,
        null,
@@ -146,7 +186,8 @@ var hrmExtension = new Extension(
 extensionList.add(hrmExtension)*/
 
 
-// biop extension
+// ------------------------ BIOP EXTENSION
+
 def biopTagList = ["v3.3.0"] 
 def biopVersionList = []
 biopTagList.each{tag->
@@ -188,3 +229,9 @@ try (FileWriter fileWriter = new FileWriter(savingFolder)) {
       .toJson(catalog, fileWriter);
 }
 println "Catalog saved to " + savingFolder
+
+
+import qupath.ext.extensionmanager.core.catalog.*
+import com.google.gson.GsonBuilder
+import com.google.gson.FieldNamingPolicy
+import java.nio.file.Paths
