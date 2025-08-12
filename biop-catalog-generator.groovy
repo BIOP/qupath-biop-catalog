618a5497831a261b21c4998502c8103812b627fd
Rémy Dornier <remy.dornier@epfl.ch>
2025-08-12 09:42:59 +0200

    Adding qupath-extension-biop 3.4.0

--- biop-catalog-generator.groovy
+++ biop-catalog-generator.groovy
@@ -190,7 +190,7 @@ extensionList.add(hrmExtension)*/
 
 // ------------------------ BIOP EXTENSION
 
-def biopTagList = ["v3.3.0"] 
+def biopTagList = ["v3.3.0", "v3.4.0"]
 def biopVersionList = []
 biopTagList.each{tag->
     var biopRelease = new Release(
